92107cf7d7f3c5a221364ebaf8028a31f06c0742
Victor Hugo <victorzevallos@protonmail.com>
2018-03-03 14:59:14 -0500

    información del contribuyente emisor

---
+++ config/company.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'ruc' => '20552103816',
+    'razon_social' => 'EMPRESA S.A.C.',
+    'nombre_comercial' => 'COMERCIAL FE',
+    'domicilio_fiscal' => 'AV. CIRCUNVALACIÓN NRO. 1340 AREQUIPA - AREQUIPA - AREQUIPA',
+];
--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/foo', function() {
+    return config('company.razon_social');
+});
