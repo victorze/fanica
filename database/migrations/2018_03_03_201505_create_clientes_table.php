324d1c4dc50be8c8be168d4a6832bb6d0aad33c7
Victor Hugo <victorzevallos@protonmail.com>
2018-03-03 15:27:09 -0500

    definición de la tabla de clientes

---
+++ database/migrations/2018_03_03_201505_create_clientes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateClientesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clientes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->char('tipo_doc', 1);
+            $table->string('numero_doc', 15);
+            $table->string('razon_social', 100);
+            $table->string('domicilio_fiscal', 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clientes');
+    }
+}
