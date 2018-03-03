08261e1a53682d41712e3d547549f9ad7568aab6
Victor Hugo <victorzevallos@protonmail.com>
2018-03-03 16:18:40 -0500

    definición de la tabla productos

---
+++ database/migrations/2018_03_03_203327_create_productos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('codigo', 30);
+            $table->string('descripcion', 250);
+            $table->string('unidad_medida', 3);
+            $table->decimal('valor_venta', 12, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('productos');
+    }
+}
