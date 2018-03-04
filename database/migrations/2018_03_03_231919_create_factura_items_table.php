e40093c87605fdfc0533c6065dc94dde5265f7fc
Victor Hugo <victorzevallos@protonmail.com>
2018-03-03 19:59:06 -0500

    definición de la tabla factura_items

---
+++ database/migrations/2018_03_03_231919_create_factura_items_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFacturaItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('factura_items', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('factura_id')->unsigned();
+            $table->integer('producto_id')->unsigned();
+            $table->double('cantidad', 12, 3);
+            $table->decimal('descuento', 12, 2);
+
+            $table->foreign('factura_id')->references('id')->on('facturas')->onDelete('cascade');
+            $table->foreign('producto_id')->references('id')->on('productos')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('factura_items');
+    }
+}
