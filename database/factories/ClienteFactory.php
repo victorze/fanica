3b1451225410f267282eaaabf57c45cfd3566a64
Victor Hugo <victorzevallos@protonmail.com>
2018-03-09 19:15:12 -0500

    cambios en factories

--- database/factories/ClienteFactory.php
+++ database/factories/ClienteFactory.php
@@ -5,7 +5,7 @@ use Faker\Generator as Faker;
 $factory->define(App\Cliente::class, function (Faker $faker) {
     return [
         'tipo_doc' => '6',
-        'numero_doc' => $faker->numberBetween($min = 20000000000, $max = 2090000000),
+        'numero_doc' => $faker->numberBetween($min = 20000000000, $max = 20900000000),
         'razon_social' => $faker->company,
         'domicilio_fiscal' => $faker->address,
     ];
--- database/factories/VentaFactory.php
+++ database/factories/VentaFactory.php
@@ -3,10 +3,9 @@
 use Faker\Generator as Faker;
 
 $factory->define(App\Venta::class, function (Faker $faker) {
-    $date = $faker->date();
     return [
-        'fecha_emision' => $date,
-        'fecha_vcto' => $date,
+        'fecha_emision' => new DateTime(),
+        'fecha_vcto' => new DateTime(),
         'tipo' => '01',
         'serie' => 'F001',
         'correlativo' => $faker->numberBetween(1, 1000),
