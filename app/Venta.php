13e98c54cc972192d830befcaf98679165ba3f35
Victor Hugo <victorzevallos@protonmail.com>
2018-03-09 19:47:27 -0500

    se agrega métodos al modelo Ventas

--- app/Venta.php
+++ app/Venta.php
@@ -15,4 +15,23 @@ class Venta extends Model
     {
         return $this->hasMany(VentaItem::class);
     }
+
+    public function baseImponible()
+    {
+        $suma = 0;
+        foreach ($this->items as $item) {
+            $suma += round($item->cantidad * $item->valor_unitario, 2);
+        }
+        return $suma;
+    }
+
+    public function igv()
+    {
+        return round($this->baseImponible() * $this->tasa_igv, 2);
+    }
+
+    public function importeTotal()
+    {
+        return $this->baseImponible() + $this->igv();
+    }
 }
